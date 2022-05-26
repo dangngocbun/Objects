0fef4c205ca01a99e4b1ec517049701bf483026a
dangngocbun <zedgunny@gmail.com>
2022-05-26 09:59:00 +0700

    first commit

---
+++ src/advance/dev/MainApp.java
@@ -0,0 +1,18 @@
+package advance.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap ten: ");
+		Object ten = sc.next();
+		System.out.println("Nhap old: ");
+		Object old = sc.nextInt();
+		System.out.println("Nhap diem: ");
+		Object diem = sc.nextDouble();
+	}
+
+}
